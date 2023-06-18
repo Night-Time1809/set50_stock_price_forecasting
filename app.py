66f10bc7ef4f0a5c9685f5db53265c37e50525a0
Night-Time1809 <chanoknun.wora@gmail.com>
2023-06-18 08:38:49 +0000

    180666-02

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def mae(model, y, preds):
     return np.mean(mae)
 
 @st.cache_resource
-def dense_model(structure, data, model_name, epoch, batch_size, save_path):
+def dense_model(structure, data, model_name, epoch, batch_size, save_path, _scaler_windows, _scaler_labels):
     tf.random.set_seed(42)
     np.random.seed(42)
     random.seed(42)
@@ -241,7 +241,7 @@ def dense_model(structure, data, model_name, epoch, batch_size, save_path):
     MAE = {"train": mae_train,
            "test": mae_test}
   
-    return train_loss, val_loss, MSE, MAE 
+    return train_loss, val_loss, MSE, MAE, _scaler_windows, _scaler_labels
 
 if st.button("Train Model"):
     now = datetime.now(tz=ZoneInfo("Asia/Bangkok"))
@@ -265,10 +265,12 @@ if st.button("Train Model"):
     inform[dt_string]["window"] = WINDOWS
     inform[dt_string]["horizon"] = HORIZONS
     inform[dt_string]["test_size"] = test_split
+    # inform[dt_string]["scaler_windows"] = scaler_windows
+    # inform[dt_string]["scaler_labels"] = scaler_labels
 
     if selected_model == "Neural Network":
         st.write("NN")
-        train_loss, val_loss, MSE, MAE = dense_model(structure=structure, data=data_train_test, model_name=model_name, epoch=EPOCH, batch_size=BATCH_SIZE, save_path=SAVE_PATH)
+        train_loss, val_loss, MSE, MAE, scaler_windows, scaler_labels = dense_model(structure=structure, data=data_train_test, model_name=model_name, epoch=EPOCH, batch_size=BATCH_SIZE, save_path=SAVE_PATH, _scaler_windows=scaler_windows, _scaler_labels=scaler_labels)
     
     elif selected_model == "LSTM":
         st.write("LSTM")
@@ -277,12 +279,14 @@ if st.button("Train Model"):
     inform[dt_string]["val_loss"] = val_loss
     inform[dt_string]["mse"] = MSE
     inform[dt_string]["mae"] = MAE
+    inform[dt_string]["scaler_windows"] = scaler_windows
+    inform[dt_string]["scaler_labels"] = scaler_labels
     pickle.dump(inform, open(f"model/{selected_stock}/information.pkl", "wb"))
 
-st.write("")
-st.markdown("## Model Evaluation")
-
 if os.path.isfile(f"{path}/{selected_stock}/information.pkl"):
+    st.write("")
+    st.markdown("## Model Evaluation")
+
     informs = pickle.load(open(f"{path}/{selected_stock}/information.pkl", "rb"))
     date = []
     time = []
@@ -378,10 +382,33 @@ if os.path.isfile(f"{path}/{selected_stock}/information.pkl"):
     ax.grid(color="grey")
     st.pyplot(fig)
 
-st.write("")
-st.markdown("## Forecasting")
-select_forecasting_model = st.selectbox("Select a model for stock price forecasting",
-                                        [f"Model {i}" for i in np.arange(1,len(informs.keys())+1)])
-forecasting_model_filename = f"{(df_compare['Date'][df_compare['Model'] == int(select_forecasting_model[-1])]).iloc[0]}_{(df_compare['Time'][df_compare['Model'] == int(select_forecasting_model[-1])]).iloc[0]}"
-forecasting_model = tf.keras.models.load_model(f"{path}/{selected_stock}/{forecasting_model_filename}")
-st.write(forecasting_model_filename)
+    st.write("")
+    st.markdown("## Forecasting")
+    select_forecasting_model = st.selectbox("Select a model for stock price forecasting",
+                                            [f"Model {i}" for i in np.arange(1,len(informs.keys())+1)])
+    forecasting_model_filename = f"{(df_compare['Date'][df_compare['Model'] == int(select_forecasting_model[-1])]).iloc[0]}_{(df_compare['Time'][df_compare['Model'] == int(select_forecasting_model[-1])]).iloc[0]}"
+    forecasting_model = tf.keras.models.load_model(f"{path}/{selected_stock}/{forecasting_model_filename}")
+    st.write(forecasting_model_filename)
+
+    forecasting_windows = (df_compare['Window Size'][df_compare['Model'] == int(select_forecasting_model[-1])]).iloc[0]
+    forecasting_horizon = (df_compare['Horizon'][df_compare['Model'] == int(select_forecasting_model[-1])]).iloc[0]
+    full_windows, full_labels = make_windows(stock_prices, forecasting_windows, forecasting_horizon)
+
+    scaler_windows_for = informs[forecasting_model_filename]["scaler_windows"]
+    scaler_labels_for = informs[forecasting_model_filename]["scaler_labels"]
+
+    full_windows_scaled = scaler_windows_for.transform(full_windows)
+    full_labels_scaled = scaler_labels_for.transform(full_labels)
+    preds_for = forecasting_model.predict(full_windows_scaled)
+    preds_for_inverse = scaler_labels_for.inverse_transform(preds_for)
+    
+    x_plot = np.arange(0, len(stock_prices))
+    x_plot_preds = np.arange(len(stock_prices), len(stock_prices)+forecasting_horizon)
+
+    fig, ax = plt.subplots()
+    fig.patch.set_alpha(0)
+    ax.plot(x_plot.reshape(-1), stock_prices.reshape(-1))
+    ax.plot(x_plot_preds.reshape(-1), preds_for_inverse[-1,:].reshape(-1))
+    st.pyplot(fig)
+
+    st.write(len(stock_prices))
